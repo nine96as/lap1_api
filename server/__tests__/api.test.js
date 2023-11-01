8873efd48d9a854de23ec45f15b348ddbb404b37
nine96as <nine96@skiff.com>
2023-11-01 12:14:12 +0000

    Accidentally format tests :)

--- server/__tests__/api.test.js
+++ server/__tests__/api.test.js
@@ -1,5 +1,5 @@
-const app = require('../app')
-const request = require('supertest')
+const app = require('../app');
+const request = require('supertest');
 const fs = require('fs');
 const path = require('path');
 
@@ -8,20 +8,19 @@ describe('api server', () => {
 
   beforeAll(() => {
     api = app.listen(5000, () => {
-      console.log('Test server running on port 5000')
-    })
-  })
+      console.log('Test server running on port 5000');
+    });
+  });
 
   afterAll((done) => {
-    console.log('Gracefully stopping test server')
-    api.close(done)
-  })
-
+    console.log('Gracefully stopping test server');
+    api.close(done);
+  });
 
   // ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
   // ⚠️ the API Structure tests are not expected to be seen in projects
   // ⚠️ They are just here to make sure that you respect the structure
-  // ⚠️ defined in the lecture 
+  // ⚠️ defined in the lecture
 
   describe('API Structure', () => {
     test('app.js exists', () => {
@@ -41,7 +40,7 @@ describe('api server', () => {
       const fileExists = fs.existsSync(file);
       expect(fileExists).toBe(true);
     });
-  })
+  });
 
   // the API Structure tests are not expected to be seen in projects ⚠️
   // They are just here to make sure that you respect the structure  ⚠️
@@ -50,37 +49,33 @@ describe('api server', () => {
 
   describe('/', () => {
     test('responds to GET / with status 200', (done) => {
-      request(api)
-        .get('/')
-        .expect(200, done)
-    })
+      request(api).get('/').expect(200, done);
+    });
 
     test('responds to GET / with a message and a description', async () => {
-      const response = await request(api).get('/')
-      expect(response.body.message).toBe('Are you reddy!')
-    })
-  })
+      const response = await request(api).get('/');
+      expect(response.body.message).toBe('Are you reddy!');
+    });
+  });
 
   describe('GET /fruits -- index', () => {
     test('responds to GET /fruits with status 200', (done) => {
-      request(api)
-        .get('/fruits')
-        .expect(200, done)
-    })
+      request(api).get('/fruits').expect(200, done);
+    });
 
     test('GET /fruits display 4 elements in the web browser', async () => {
-      const response = await request(api).get('/fruits')
-      expect(response.body.length).toBe(4)
-    })
-  })
+      const response = await request(api).get('/fruits');
+      expect(response.body.length).toBe(4);
+    });
+  });
 
   describe('GET /fruits/:id -- show', () => {
     test('responds to GET /fruits/1 with status 200', (done) => {
       request(api)
         .get('/fruits/4')
         .expect(200)
-        .expect({ id: 4, name: 'date' }, done)
-    })
+        .expect({ id: 4, name: 'date' }, done);
+    });
 
     test('responds to a unknown goat id with a 404', (done) => {
       request(api)
@@ -88,65 +83,65 @@ describe('api server', () => {
         .expect(404)
         .expect({ error: 'Fruit with id 42 not found' }, done);
     });
-  })
+  });
 
   describe('POST /fruits', () => {
     test('responds to posts /fruits with status 201', (done) => {
-      const testData = { name: "The shark from jaws" }
+      const testData = { name: 'The shark from jaws' };
       request(api)
         .post('/fruits')
         .send(testData)
         .set('Accept', 'application/json')
         .expect(201)
-        .expect({ ...testData, id: 5 }, done)
+        .expect({ ...testData, id: 5 }, done);
     });
 
     test('throws a 422 if update unsuccessful', (done) => {
-      const testData = { age: 99 }
+      const testData = { age: 99 };
 
       request(api)
         .post('/fruits')
         .send(testData)
         .set('Accept', 'application/json')
         .expect(422)
-        .expect({ error: 'you need a name to create a fruit' }, done)
+        .expect({ error: 'you need a name to create a fruit' }, done);
     });
-  })
+  });
 
   describe('PATCH /fruits/:id -- update', () => {
     test('responds to PATCH /fruits/1 with status 200', (done) => {
-      const testData = { name: "meh" }
+      const testData = { name: 'meh' };
 
       request(api)
         .patch('/fruits/2')
         .send(testData)
         .set('Accept', 'application/json')
         .expect(200)
-        .expect({ ...testData, id: 2 }, done)
-    })
+        .expect({ ...testData, id: 2 }, done);
+    });
 
     test('422 - if no name in the body', (done) => {
-      const testData = { age: 2 }
+      const testData = { age: 2 };
 
       request(api)
         .patch('/fruits/2')
         .send(testData)
         .set('Accept', 'application/json')
         .expect(422)
-        .expect({ error: 'You need to specify the name' }, done)
-    })
+        .expect({ error: 'You need to specify the name' }, done);
+    });
 
     test('404 - cannot update an unexisting shark', (done) => {
-      const testData = { name: "joker" }
+      const testData = { name: 'joker' };
 
       request(api)
         .patch('/fruits/7')
         .send(testData)
         .set('Accept', 'application/json')
         .expect(404)
-        .expect({ error: 'cannot update missing fruit' }, done)
-    })
-  })
+        .expect({ error: 'cannot update missing fruit' }, done);
+    });
+  });
 
   describe('DELETE', () => {
     test('responds to delete /fruits/:id with status 204', (done) => {
@@ -161,7 +156,6 @@ describe('api server', () => {
     test('handles last id', (done) => {
       request(api).delete('/fruits/5').expect(204);
       request(api).delete('/fruits/1').expect(404, done);
-
     });
-  })
-})
+  });
+});
