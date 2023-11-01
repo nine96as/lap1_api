28b32a09d3e41a80d5880359037174512da62f32
nine96as <nine96@skiff.com>
2023-11-01 12:40:45 +0000

    Add home page get request

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const cors = require('cors');
+
+const logger = require('./logger');
+
 const app = express();
 
+// middleware section
+app.use(cors());
+app.use(express.json());
+app.use(logger);
+
+// home page
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(200).send('Are you reddy!');
 });
+
+// app.get(/fruits)
 
 module.exports = app;
